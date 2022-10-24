41018d15e311fc46e153a9b728c6b0b1d9692c46
Andres Godoy <andresegm@gmail.com>
2022-10-24 09:43:42 -0600

    Made changes to pizza interface

--- packages/server/src/application/providers/pizzas/pizza.provider.types.ts
+++ packages/server/src/application/providers/pizzas/pizza.provider.types.ts
@@ -1,7 +1,8 @@
+//import { ObjectId } from "mongodb";
 export interface Pizza {
   id: string;
   name: string;
   description: string;
-  toppingIds: string[];
+  toppingIds: any;
   imgSrc: string;
 }
--- packages/server/src/entities/pizza.ts
+++ packages/server/src/entities/pizza.ts
@@ -1,14 +1,16 @@
-import { Document } from 'mongodb';
+import { ObjectId, Document } from 'mongodb';
 import { Pizza } from '../application/providers/pizzas/pizza.provider.types';
 
-interface PizzaDocument extends Document, Omit<Pizza, 'id'> {}
+interface PizzaDocument extends Document, Omit<Pizza, 'id'> {
+  toppingIds: ObjectId[];
+}
 
 const toPizzaObject = (pizza: PizzaDocument): Pizza => {
   return {
     id: pizza._id.toHexString(),
     name: pizza.name,
     description: pizza.description,
-    toppingIds: pizza.toppingIds,
+    toppingIds: pizza.toppingIds.map((toppingId) => toppingId.toHexString()),
     imgSrc: pizza.imgSrc,
   };
 };
