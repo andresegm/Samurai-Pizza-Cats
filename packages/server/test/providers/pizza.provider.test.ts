da63f5537aef3db5312b6ba492fc724d03c4c3b9
Andres Godoy <andresegm@gmail.com>
2022-11-04 10:55:56 -0600

    add deletePizza tests to pizza provider test file

--- packages/server/test/providers/pizza.provider.test.ts
+++ packages/server/test/providers/pizza.provider.test.ts
@@ -35,6 +35,7 @@ describe('pizzaProvider', (): void => {
       expect(result).toEqual([mockPizza]);
     });
   });
+
   describe('createPizza', (): void => {
     const validPizza = createMockPizzaDocument({
       name: 'test pizza',
@@ -65,6 +66,29 @@ describe('pizzaProvider', (): void => {
       });
 
       expect(result).toEqual(toPizzaObject(validPizza));
+    });
+  });
+
+  describe('deletePizza', (): void => {
+    beforeEach(() => {
+      reveal(stubPizzaCollection).findOneAndDelete.mockImplementation(() => ({ value: mockPizzaDocument }));
+    });
+    test('should call findOneAndDelete once', async () => {
+      await pizzaProvider.deletePizza(mockPizza.id);
+
+      expect(stubPizzaCollection.findOneAndDelete).toHaveBeenCalledTimes(1);
+    });
+
+    test('should throw an error if findOneAndDelete returns null for value', async () => {
+      reveal(stubPizzaCollection).findOneAndDelete.mockImplementation(() => ({ value: null }));
+
+      await expect(pizzaProvider.deletePizza(mockPizza.id)).rejects.toThrow(new Error('Could not delete the pizza'));
+    });
+
+    test('should return an id', async () => {
+      const result = await pizzaProvider.deletePizza(mockPizza.id);
+
+      expect(result).toEqual(mockPizza.id);
     });
   });
 });
